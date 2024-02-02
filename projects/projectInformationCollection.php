cc06e2689a7e1ec2fb86013f670f705bbe6d79cb
Burnt <mauricedix999@hotmail.com>
2024-02-02 19:22:05 +1030

    fixed external website links

--- projects/projectInformationCollection.php
+++ projects/projectInformationCollection.php
@@ -8,28 +8,28 @@ array_push($projectInformationCollection,
         "Halo Checkpoint Manager", 
         new StringArray("C++", "C#", ".NET", "WPF", "MVVM framework", "DirectX", "WinAPI", "DLL injection", "interprocess communication", "Reverse engineering", "Application development", "project architecture", "GUI architecture", "object lifetime management", "open source development", "real-time application", "automated tests"), 
         file_get_contents('markdown/projects/HCM.md'), 
-        "github.com/Burnt-o/HaloCheckpointManager"
+        "https://github.com/Burnt-o/HaloCheckpointManager"
     ),
     new ProjectInformation(
         "CEER",
         "Halo CE Enemy Randomiser", 
         new StringArray("C++", "DirectX", "WinAPI", "DLL injection", "Reverse engineering", "Application development", "project architecture", "GUI architecture", "object lifetime management", "open source development", "real-time application"), 
         file_get_contents('markdown/projects/CEER.md'), 
-        "github.com/Burnt-o/CEER"
+        "https://github.com/Burnt-o/CEER"
     ),
     new ProjectInformation(
         "com",
         "Maurice-Codes.com", 
         new StringArray("HTML", "CSS", "PHP", "site design", "apache web server", "DNS", "embedded Linux system"), 
         file_get_contents('markdown/projects/Maurice-Codes.md'), 
-        "github.com/Burnt-o/maurice-codes"
+        "https://github.com/Burnt-o/maurice-codes"
     ),
     new ProjectInformation(
         "autosplitter",
         "MCC Autosplitter", 
         new StringArray("C#", "reverse engineering", "scripting language", "accurate frame-synchronous timing", "collaborative development"), 
         file_get_contents('markdown/projects/MCC Autosplitter.md'), 
-        "github.com/Burnt-o/MCC_autosplitter"
+        "https://github.com/Burnt-o/MCC_autosplitter"
     )
 );
 
