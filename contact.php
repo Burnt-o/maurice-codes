8dbc5f57b55ea01aebb01673f94d79df245f5f14
Burnt <mauricedix999@hotmail.com>
2024-02-05 00:13:44 +1030

    added contact content

--- contact.php
+++ contact.php
@@ -7,9 +7,44 @@
         </div>
     </div>
     <div id="contact-container">
+
+
+
         <div id="contact-content">
-                <h2 class="title">Contact</h2>
+            <h2 class="title">Contact</h2>
+
+        <div class="contact-grid-wrapper">
+            <div class="contact-grid-container">
+                <span class = "icon-link-pair">
+                    <img class="social-icon" src="assets/images/icon/social-email-48.png" alt="">
+                    <a class="social-link" href="mailto: maurice@burnt.codes" target="_blank">Email</a>
+                </span>
                 
+                <span class = "icon-link-pair">
+                    <img class="social-icon" src="assets/images/icon/social-discord-48.png" alt="">
+                    <a class="social-link" href="https://discordapp.com/users/158097025387200514" target="_blank">Discord</a>
+                </span>
+
+                <span class = "icon-link-pair">
+                    <img class="social-icon" src="assets/images/icon/social-youtube-48.png" alt="">
+                    <a class="social-link" href="https://www.youtube.com/channel/UCSTdbFgKprZ5X0d2i0AAPjw" target="_blank">Youtube</a>
+                </span>
+            </div>
+        </div>
+
+            <div class="contact-text"> 
+                <p>For employment-related enquiries, contact me by email at&nbsp;<a href="mailto: maurice@burnt.codes" target="_blank">maurice@burnt.codes</a> </p>
+            </div>
+
+
+
+            <div class="contact-text"> 
+                <p>To report bugs, request features or discuss collaborating on my projects, get in touch with me on Discord 
+                    - either by pinging me directly&nbsp;<a href="https://discordapp.com/users/158097025387200514" target="_blank">@burntkurisu</a>, 
+                or by hopping into my&nbsp;<a href="https://discord.gg/PXHpSJyrdE" target="_blank">tool discussion server</a></p>
+            </div>
+            
+
         </div>
     </div>
 
